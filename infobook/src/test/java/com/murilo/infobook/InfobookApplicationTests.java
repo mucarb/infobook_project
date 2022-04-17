4bd58297de7157cabbeb48c520d64a3fd20c397f
mucarb <murilo.rb60@gmail.com>
2022-04-17 20:26:26 -0300

    Projeto iniciado

---
+++ infobook/src/test/java/com/murilo/infobook/InfobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilo.infobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
